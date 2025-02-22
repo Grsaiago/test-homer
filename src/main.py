fac829852703aaa54d398016ac5991776a05f175
Gabriel Saiago <grsaiago@gmail.com>
2025-02-22 14:29:39 -0300

    refactor(main): to use the exported typed env and db_layer objects

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 
-import sys
-
+from langchain.globals import set_debug
 from langchain_core.messages import AnyMessage, HumanMessage
 from langchain_core.runnables import Runnable, RunnableConfig
 from langchain_ollama import ChatOllama
-from langchain.globals import set_debug
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph import MessagesState, StateGraph
 from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode
 from psycopg_pool import ConnectionPool
-from project_types.state_types import State
 
 import internal_tools
-from project_types.env_types import EnvSetupException, TypedEnvs
+from project_types.database_types import (
+    database_layer,
+)  # Has the sqlAlchemy connection as well as a pgPool
+from project_types.env_types import (
+    envs,
+)  # this import validates if all envs exist and places them in a typed env object
+from project_types.state_types import State
 from prompt import create_model_prompt
 from sentiment_analyzer import sentiment_analyzer
 
@@ -95,84 +98,62 @@ def sentiment_analysis(state: State) -> MessagesState:
     return state
 
 
-envs: TypedEnvs
-try:
-    envs = TypedEnvs.load_envs()
-except EnvSetupException as err:
-    print("Failed due to lack of envs setup:")
-    for error in err.errors:
-        print(f"- {error}")
-    sys.exit(-1)
-
-
 def main():
     # set debugging for langGraph
     set_debug(envs.debug)
     print(f"O valor de debug é : {envs.debug}")
 
-    db_connection_kwargs = {
-        "autocommit": True,
-        "prepare_threshold": 0,
-    }
-    db_connection_string = f"postgresql://{envs.db_user}:{envs.db_passwd}@{envs.db_host}:5432/{envs.db_name}"
-    with ConnectionPool(
-        conninfo=db_connection_string,
-        max_size=20,
-        kwargs=db_connection_kwargs,
-    ) as db_pool:
-        # memory setup
-        memory = PostgresSaver(db_pool)
-        memory.setup()
+    # memory setup
+    memory = PostgresSaver(database_layer.get_lang_graph_pool())
+    memory.setup()
 
-        # setup tools (aqui começa a codebase bilingue xD)
-        update_state_tools = [internal_tools.atualizar_quartos]
+    # setup tools (aqui começa a codebase bilingue xD)
+    update_state_tools = [internal_tools.atualizar_quartos]
 
-        # Model/Tooling initialization
-        chatbot_model = ChatOllama(model="llama3.2").bind_tools(update_state_tools)
-        tool_node = ToolNode(tools=update_state_tools)
+    # Model/Tooling initialization
+    chatbot_model = ChatOllama(model="llama3.2").bind_tools(update_state_tools)
+    tool_node = ToolNode(tools=update_state_tools)
 
-        # Graph compile
-        graph_builder = StateGraph(State)
-        graph = (
-            graph_builder.add_node("agent", lambda state: chatbot(state, chatbot_model))
-            .add_node("tools", tool_node)
-            .add_node("sentiment_node", sentiment_analysis)
-            .set_entry_point("agent")
-            .add_conditional_edges(
-                "agent",
-                should_continue,
-                {"tools": "tools", "sentiment_node": "sentiment_node"},
-            )  # decide between tool usage ot not
-            .add_edge("tools", "agent")  # after tools, go back to agent
-            .set_finish_point("sentiment_node")
-            .compile(checkpointer=memory)
-        )
+    # Graph compile
+    graph_builder = StateGraph(State)
+    graph = (
+        graph_builder.add_node("agent", lambda state: chatbot(state, chatbot_model))
+        .add_node("tools", tool_node)
+        .add_node("sentiment_node", sentiment_analysis)
+        .set_entry_point("agent")
+        .add_conditional_edges(
+            "agent",
+            should_continue,
+            {"tools": "tools", "sentiment_node": "sentiment_node"},
+        )  # decide between tool usage ot not
+        .add_edge("tools", "agent")  # after tools, go back to agent
+        .set_finish_point("sentiment_node")
+        .compile(checkpointer=memory)
+    )
 
+    while True:
+        user_id = input("Qual o seu nome?: ")
         while True:
-            user_id = input("Qual o seu nome?: ")
-            while True:
-                try:
-                    user_input = input("User: ")
-                    match user_input.lower():
-                        case "q" | "quit" | "exit":
-                            print("Byeeee")
-                            return
-                        case "novo_nome":
-                            print("Troca de nome")
-                            break
+            try:
+                user_input = input("User: ")
+                match user_input.lower():
+                    case "q" | "quit" | "exit":
+                        print("Byeeee")
+                        return
+                    case "novo_nome":
+                        print("Troca de nome")
+                        break
 
-                    # get the initial state for a given id and append the current user message as the last message
-                    initial_state = get_state_by_id(user_id, memory)
-                    initial_state["messages"] = [HumanMessage(content=user_input)]
-                    print("INITIAL STATE É: ", initial_state)
+                # get the initial state for a given id and append the current user message as the last message
+                initial_state = get_state_by_id(user_id, memory)
+                initial_state["messages"] = [HumanMessage(content=user_input)]
+                print("INITIAL STATE É: ", initial_state)
 
-                    user_config: RunnableConfig = {
-                        "configurable": {"thread_id": user_id}
-                    }
-                    stream_graph_updates(graph, initial_state, user_config)
-                except Exception as e:
-                    print("System: Something went wrong " + e.__str__())
-                    break
+                user_config: RunnableConfig = {"configurable": {"thread_id": user_id}}
+                stream_graph_updates(graph, initial_state, user_config)
+            except Exception as e:
+                print("System: Something went wrong " + e.__str__())
+                break
 
 
 def get_state_by_id(id: str, memory_layer: PostgresSaver) -> State:
